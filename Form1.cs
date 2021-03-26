82abb652985c2a2d432e0730717d9247e8b66ac6
dimass2580 <dimass2580@gmail.com>
2021-03-26 22:09:32 +0300

    Add Popup notification about suspend

--- Form1.cs
+++ Form1.cs
@@ -24,6 +24,8 @@ namespace Sleeper
 
         DateTime selected = new DateTime();
         DateTime time = new DateTime();
+        bool selectedFlag = false;
+        bool showed = false;
 
         private void Form1_Shown(object sender, EventArgs e)
         {
@@ -34,24 +36,57 @@ namespace Sleeper
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            DateTime time = new DateTime();
+            
             time = DateTime.Now;
             Hour.Text = time.Hour.ToString();
             Minute.Text = time.Minute.ToString();
             Second.Text = time.Second.ToString();
-            if (time.ToLongTimeString().Equals(selected.ToLongTimeString()))
+            
+            if (selectedFlag)
             {
-                MessageBox.Show("Перевод пк в режим сна");
-                Thread.Sleep(1000);
-                hibernate();
+                var popupTime = selected.Subtract(new TimeSpan(0, 15, 0));
+                if (time.ToLongTimeString().Equals(popupTime.ToLongTimeString()) && !showed)
+                {
+                    notifyIcon1.BalloonTipText = $"Компьютер выключится через 15 минут";
+                    if (notifyIcon1.Visible)
+                        notifyIcon1.ShowBalloonTip(5);
+                    else
+                    {
+                        notifyIcon1.Visible = true;
+                        notifyIcon1.ShowBalloonTip(5);
+                        notifyIcon1.Visible = false;
+                    }
+                    showed = true;
+                }
+
+                if (time.ToLongTimeString().Equals(selected.ToLongTimeString()))
+                {
+                    MessageBox.Show("Перевод пк в режим сна через 1 минуту");
+                    Thread.Sleep(60000);
+                    showed = false;
+                    hibernate();
+                }
             }
+           
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            selected = DateTime.Parse($"{SetHour.Value}:{SetMinute.Value}:{SetSec.Value}");
-            MessageBox.Show("Время выбрано");
-            Console.WriteLine(selected.ToShortTimeString());
+            if(!selectedFlag)
+            {
+                selected = DateTime.Parse($"{SetHour.Value}:{SetMinute.Value}:{SetSec.Value}");
+                MessageBox.Show("Время выбрано");
+                Console.WriteLine(selected.ToShortTimeString());
+                selectedFlag = true;
+                showed = false;
+                setTime.Text = "Отключить";
+            }
+            else
+            {
+                showed = false;
+                selectedFlag = false;
+                setTime.Text = "Задать";
+            }
         }
         
         private void hibernate()
@@ -79,6 +114,7 @@ namespace Sleeper
             { 
                 ShowInTaskbar = false;
                 notifyIcon1.Visible = true;
+                notifyIcon1.Text = "Sleeper работает в трее";
                 notifyIcon1.ShowBalloonTip(5);
             }
         }
